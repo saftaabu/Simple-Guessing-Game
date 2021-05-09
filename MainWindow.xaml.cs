5cb0bcf190878e895959fa952b13e4f769de0ce3
Syed Aftaabuddin <syedaftab299@gmail.com>
2021-05-09 13:05:25 -0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Midterm_Aftaabuddin_Syed
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        static Random r = new Random();
+        int number = r.Next(10);
+        int guessNum;
+        int win = 10;
+        int guess = 1;
+
+        private void btn_click(object sender, RoutedEventArgs e)
+        {
+            //guessNum = Convert.ToInt32(txtNumber.Text);
+            lblOutput.Content = number;
+        }
+    }
+}
